0d2de6411ca48399507c6b11cf8a4a462963039a
Glenn Block <glenn.block@gmail.com>
2018-03-16 12:46:40 -0700

    Update codefellows.js

--- codefellows.js
+++ codefellows.js
@@ -4,6 +4,7 @@
 module.exports = function(context, cb) {
   
   console.log(context.body);
+  console.log("I was edited");
   console.log(context.secrets.slack_key);
   cb(null, { hello: context.query.name || 'Anonymous' });
 };
