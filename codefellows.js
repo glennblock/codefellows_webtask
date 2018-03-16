bdcd0c60a5d995997c42c159bf974464d1a774d1
Glenn Block <glenn.block@gmail.com>
2018-03-16 14:46:08 -0500

    Updating codefellows.js

--- codefellows.js
+++ codefellows.js
@@ -2,5 +2,8 @@
 * @param context {WebtaskContext}
 */
 module.exports = function(context, cb) {
+  
+  console.log(context.body);
+  console.log(context.secrets.slack_key);
   cb(null, { hello: context.query.name || 'Anonymous' });
 };
